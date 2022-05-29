406cbcd579ce629d0b2061dd361d606dab2a918a
Teni Mathew <tenimathew@gmail.com>
2022-05-29 18:04:12 +0530

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: "tenimathew.github.io", // Usually your GitHub org/user name.
   projectName: "oracle", // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
